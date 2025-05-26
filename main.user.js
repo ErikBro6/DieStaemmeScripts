ba7dd129b4f9bcce22b65de55bf89ac608245d2f
ErikBro6 <160338690+ErikBro6@users.noreply.github.com>
2025-05-26 20:36:35 +0200

    forgot to adjust version

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SpeckMichs Die Stämme Tool Collection
 // @namespace    https://github.com/deinname/ds-tools
-// @version      1.0
+// @version      1.1
 // @description  Erweitert die Die Stämme Erfahrung mit einigen Tools und Skripten
 // @author       SpeckMich
 // @connect      raw.githubusercontent.com
