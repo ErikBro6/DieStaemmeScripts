3dff099b9ed3e640a192eb9978198a005cb22190
ErikBro6 <160338690+ErikBro6@users.noreply.github.com>
2025-10-23 21:18:39 +0200

    Version

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SpeckMichs Die Stämme Tool Collection
 // @namespace    https://github.com/deinname/ds-tools
-// @version      3.0.3
+// @version      3.0.4
 // @description  Erweitert die Die Stämme Erfahrung mit einigen Tools und Skripten
 // @author       SpeckMich
 // @connect      raw.githubusercontent.com
