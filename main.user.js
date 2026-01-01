5ae80a0a43d8a1d7a5d8ea75e586ad2c938c9ff3
ErikBro6 <160338690+ErikBro6@users.noreply.github.com>
2026-01-01 15:48:55 +0100

    Refactored MassScavenger and Added Notification Module

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SpeckMichs Die Stämme Tool Collection
 // @namespace    https://github.com/EmoteBot6
-// @version      3.3.5
+// @version      3.3.6
 // @description  Erweitert die Die Stämme Erfahrung mit einigen Tools und Skripten
 // @author       SpeckMich
 // @connect      raw.githubusercontent.com
